2a35ca38d7f2a7b36d910f4301005fbec8a98fc9
Your Name <hudsasharma10@gmail.com>
2022-06-05 11:17:19 +0530

    Ashish changes|

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -256,7 +256,6 @@ function Home() {
           </div>
         </div>
       </section>
-      <br />
       <section
         className="divider parallax layer-overlay overlay-dark-6"
         data-bg-img="assets/images/bg/bg4.jpg"
@@ -284,7 +283,163 @@ function Home() {
         </div>
       </section>
 
-      <section className="divider parallax">gskfdg</section>
+      <br />
+      <section className="bg-lighter">
+        <div className="container">
+          <div className="section-content">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="widget">
+                  <h5 className="widget-title line-bottom">
+                    <b className="text-theme-colored">Training Highlights</b>
+                  </h5>
+                  <ul className="list list-divider list-border">
+                    <li>
+                      <a href="/">
+                        <i className="fa fa-check-square-o mr-10 text-black-light" />
+                        {' '}
+                        6 months hands on practical technical training
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/">
+                        <i className="fa fa-check-square-o mr-10 text-black-light" />
+                        {' '}
+                        Become and expert in futuristic technologies like Linux,
+                        Ansible and Automation, AWS, AZURE, DevOps, Docker and Kubernetes, Jenkins,
+                        GIT HUB, terraform, CI-CD pipleine.
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/">
+                        <i className="fa fa-check-square-o mr-10 text-black-light" />
+                        {' '}
+                        Regular assesment and analysis of candidates.
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/">
+                        <i className="fa fa-check-square-o mr-10 text-black-light" />
+                        {' '}
+                        Interview preparation
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/">
+                        <i className="fa fa-check-square-o mr-10 text-black-light" />
+                        {' '}
+                        Exposure of company environment.
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/">
+                        <i className="fa fa-check-square-o mr-10 text-black-light" />
+                        {' '}
+                        Interview conduction.
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/">
+                        <i className="fa fa-check-square-o mr-10 text-black-light" />
+                        {' '}
+                        ISA norms to be started after candidate gets placed.
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/">
+                        <i className="fa fa-check-square-o mr-10 text-black-light" />
+                        {' '}
+                        Meet our hiring partners
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <h3 className="line-bottom line-height-1 mt-0 mt-sm-30">
+                  Pay after placement and ISA agreement.
+                </h3>
+                <p className="mb-30">
+                  Pay after you get placed. Grras solutions pvt. ltd.
+                  provides guidence for Job placement.
+                  We will be there with you at everys step of your hiurney.
+                </p>
+                <div className="row">
+                  <div className="col-xs-12 col-sm-6 col-md-6">
+                    <div className="icon-box p-0 mb-30">
+                      <a
+                        className="icon icon-sm pull-left sm-pull-none flip bg-theme-colored mb-sm-15 mb-0 mr-10"
+                        href="/"
+                      >
+                        <i className="fa fa-clock-o  text-white font-36" />
+                      </a>
+                      <h4 className="icon-box-title m-0 mb-5">
+                        TIME FLEXIBILITY
+                      </h4>
+                      <p className="text-gray mb-5">
+                        With a team of more than 100 + trainers we can provide
+                        any time slot and any option
+                      </p>
+                    </div>
+                  </div>
+                  <div className="col-xs-12 col-sm-6 col-md-6">
+                    <div className="icon-box p-0 mb-30">
+                      <a
+                        className="icon icon-sm pull-left sm-pull-none flip bg-theme-colored mb-sm-15 mb-0 mr-10"
+                        href="/"
+                      >
+                        <i className="fa fa-credit-card text-white font-36" />
+                      </a>
+                      <h4 className="icon-box-title m-0 mb-5">
+                        AFFORDABLE FEES
+                      </h4>
+                      <p className="text-gray mb-5">
+                        We are not cheap , but we provide quality training with
+                        justified fees structures.
+                      </p>
+                    </div>
+                  </div>
+                  <div className="col-xs-12 col-sm-6 col-md-6">
+                    <div className="icon-box p-0 mb-sm-30">
+                      <a
+                        className="icon icon-sm pull-left sm-pull-none flip bg-theme-colored mb-sm-15 mb-0 mr-10"
+                        href="/"
+                      >
+                        <i className="fa fa-certificate text-white font-36" />
+                      </a>
+                      <h4 className="icon-box-title m-0 mb-5">
+                        Certified Consultent
+                      </h4>
+                      <p className="text-gray mb-5">
+                        Learning various technologies under the supervision of
+                        well trained , expereinced and certified trainers of
+                        GRRAS
+                      </p>
+                    </div>
+                  </div>
+                  <div className="col-xs-12 col-sm-6 col-md-6">
+                    <div className="icon-box p-0">
+                      <a
+                        className="icon icon-sm pull-left sm-pull-none flip bg-theme-colored mb-sm-15 mb-0 mr-10"
+                        href="/"
+                      >
+                        <i className="fa fa-expeditedssl  text-white font-36" />
+                      </a>
+                      <h4 className="icon-box-title m-0 mb-5">
+                        FULL HANDS-ON TRAINING
+                      </h4>
+                      <p className="text-gray mb-5">
+                        Grras focuses on learning modules with practical
+                        examples ,live environment and real time projects
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       <section id="contact" className="bg-silver-light">
         <div className="container pb-sm-10">
           <div className="row">
